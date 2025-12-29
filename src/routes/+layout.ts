11e2c300ed4bc06a2d8301e4504579a9055e65e7
Max Petretta <max@maxpetretta.com>
2025-12-29 16:10:07 -0500

    fix: disable prerender to resolve build errors

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,7 +2,7 @@ import { dev } from "$app/environment";
 import { inject } from "@vercel/analytics";
 import { injectSpeedInsights } from "@vercel/speed-insights/sveltekit";
 
-export const prerender = true;
+export const prerender = false;
 
 inject({ mode: dev ? "development" : "production" });
 injectSpeedInsights();
